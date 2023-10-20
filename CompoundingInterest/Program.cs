ae462301ebcba41d5604e6ad3f6bf10ac555dcfb
CorySonderman <sonderman2000@yahoo.com>
2023-10-20 08:48:10 -0500

    .

---
+++ CompoundingInterest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CompoundingInterest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!!");
+        }
+    }
+}
